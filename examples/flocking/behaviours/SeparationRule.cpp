dbbe5501b890e2f5ea6a9837331bce38d77a2eaa
SpencerBohl <spencer.bohl@mymail.champlain.edu>
2022-09-13 14:16:46 -0400

    Finished SeparationRule and MouseInfluenceRule and started WindRule

--- examples/flocking/behaviours/SeparationRule.cpp
+++ examples/flocking/behaviours/SeparationRule.cpp
@@ -11,19 +11,39 @@ Vector2 SeparationRule::computeForce(const std::vector<Boid*>& neighborhood, Boi
     float desiredDistance = desiredMinimalDistance;
 
     // todo: implement a force that if neighbor(s) enter the radius, moves the boid away from it/them
-    if (!neighborhood.empty()) {
+    //If the neighborhood is not empty check positions
+    if (!neighborhood.empty()) 
+    {
+        //Get position of current boid for later and start counting the close flockmates
         Vector2 position = boid->transform.position;
         int countCloseFlockmates = 0;
+        //Iterate through the neighborhood
         for (int i = 0; i < neighborhood.size(); i++)
         {
+            //Store current neighbor position in a temp vector
             Vector2 temp = neighborhood[i]->getPosition();
-            totalPosition += temp;
+            //Get the distance from the neighbor and check the magnitude
+            //If it is closer than our desired distance count it as a close flockmate and add temp to the total position
+            if (Vector2::getDistance(position,temp) <= desiredMinimalDistance)
+            {
+               countCloseFlockmates++;
+               totalPosition += temp;
+            }
         }
-        
+        //If we have close flockmates calculate the Center of Mass and the seperating force
+        if (countCloseFlockmates != 0)
+        {
+            centerOfMass = (totalPosition / countCloseFlockmates);
+            separatingForce = centerOfMass - position;
+            separatingForce = Vector2::normalized(separatingForce);
+            separatingForce *= -1.0f;
+        }
+    }//If the neighborhood is empty make sure that the separating force is zero
+    else if (neighborhood.empty())
+    {
+        separatingForce = Vector2::zero();
     }
-
-    separatingForce = Vector2::normalized(separatingForce);
-
+ 
     return separatingForce;
 }
 
